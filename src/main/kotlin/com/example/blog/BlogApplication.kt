a5c654a0fa385d3aa2737c737b5dd27946fd51c6
SubbotinDmitriy <jetixhunterrollx@gmail.com>
2018-03-24 14:58:14 +0700

    set

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.ConfigurableApplicationContext
 
-
+lateinit var context: ConfigurableApplicationContext
 @Controller
 @SpringBootApplication
 @Configuration
@@ -23,49 +23,46 @@ class BlogApplication {
     @RequestMapping("/")
     @ResponseBody
     fun home(): String {
+        val repository = context.getBean(CustomerRepository::class.java)
+
+        // save a couple of customers
+        repository.save(Customer("Jack", "Bauer"))
+        repository.save(Customer("Chloe", "O'Brian"))
+        repository.save(Customer("Kim", "Bauer"))
+        repository.save(Customer("David", "Palmer"))
+        repository.save(Customer("Michelle", "Dessler"))
+
+        // fetch all customers
+        val customers = repository.findAll()
+        println("Customers found with findAll():")
+        println("-------------------------------")
+        for (customer in customers) {
+            println(customer)
+        }
+        println()
+
+        // fetch an individual customer by ID
+        val customer = repository.findById(1L)
+        println("Customer found with findOne(1L):")
+        println("--------------------------------")
+        println(customer)
+        println()
+
+        // fetch customers by last name
+        val bauers = repository.findByLastName("Bauer")
+        println("Customer found with findByLastName('Bauer'):")
+        println("--------------------------------------------")
+        for (bauer in bauers) {
+            println(bauer)
+        }
+        context.close()
+
         return "Hello World!"
     }
 }
 
 fun main(args: Array<String>) {
-    val context: ConfigurableApplicationContext = runApplication<BlogApplication>(*args)
-
-
-//    val context = SpringApplication.run(Application::class.java)
-    val repository = context.getBean(CustomerRepository::class.java)
-
-    // save a couple of customers
-    repository.save(Customer("Jack", "Bauer"))
-    repository.save(Customer("Chloe", "O'Brian"))
-    repository.save(Customer("Kim", "Bauer"))
-    repository.save(Customer("David", "Palmer"))
-    repository.save(Customer("Michelle", "Dessler"))
-
-    // fetch all customers
-    val customers = repository.findAll()
-    println("Customers found with findAll():")
-    println("-------------------------------")
-    for (customer in customers) {
-        println(customer)
-    }
-    println()
-
-    // fetch an individual customer by ID
-    val customer = repository.findById(1L)
-    println("Customer found with findOne(1L):")
-    println("--------------------------------")
-    println(customer)
-    println()
-
-    // fetch customers by last name
-    val bauers = repository.findByLastName("Bauer")
-    println("Customer found with findByLastName('Bauer'):")
-    println("--------------------------------------------")
-    for (bauer in bauers) {
-        println(bauer)
-    }
-
-    context.close()
+    context = runApplication<BlogApplication>(*args)
 }
 
 
