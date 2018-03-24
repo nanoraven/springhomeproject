e15056373b952110529dcdfe6c3790186db5b924
SubbotinDmitriy <jetixhunterrollx@gmail.com>
2018-03-24 14:40:08 +0700

    set

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -5,16 +5,18 @@ import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RequestMapping
-import jdk.nashorn.internal.objects.NativeArray.forEach
 import com.example.blog.hello.Customer
 import com.example.blog.hello.CustomerRepository
 import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
-import org.springframework.context.annotation.Bean
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.ConfigurableApplicationContext
 
 
 @Controller
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 class BlogApplication {
     private val log = LoggerFactory.getLogger(BlogApplication::class.java)
 
@@ -23,49 +25,47 @@ class BlogApplication {
     fun home(): String {
         return "Hello World!"
     }
+}
 
-    @Bean
-    fun demo(repository: CustomerRepository): CommandLineRunner {
-        return CommandLineRunner { args ->
-            // save a couple of customers
-            repository.save(Customer("Jack", "Bauer"))
-            repository.save(Customer("Chloe", "O'Brian"))
-            repository.save(Customer("Kim", "Bauer"))
-            repository.save(Customer("David", "Palmer"))
-            repository.save(Customer("Michelle", "Dessler"))
+fun main(args: Array<String>) {
+    val context: ConfigurableApplicationContext = runApplication<BlogApplication>(*args)
 
-            // fetch all customers
-            log.info("Customers found with findAll():")
-            log.info("-------------------------------")
-            for (customer in repository.findAll()) {
-                log.info(customer.toString())
-            }
-            log.info("")
 
-            // fetch an individual customer by ID
-            repository.findById(1L)
-                    .ifPresent { customer ->
-                        log.info("Customer found with findById(1L):")
-                        log.info("--------------------------------")
-                        log.info(customer.toString())
-                        log.info("")
-                    }
+//    val context = SpringApplication.run(Application::class.java)
+    val repository = context.getBean(CustomerRepository::class.java)
 
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):")
-            log.info("--------------------------------------------")
-            repository.findByLastName("Bauer").forEach { bauer -> log.info(bauer.toString()) }
-            // for (Customer bauer : repository.findByLastName("Bauer")) {
-            // 	log.info(bauer.toString());
-            // }
-            log.info("")
-        }
+    // save a couple of customers
+    repository.save(Customer("Jack", "Bauer"))
+    repository.save(Customer("Chloe", "O'Brian"))
+    repository.save(Customer("Kim", "Bauer"))
+    repository.save(Customer("David", "Palmer"))
+    repository.save(Customer("Michelle", "Dessler"))
+
+    // fetch all customers
+    val customers = repository.findAll()
+    println("Customers found with findAll():")
+    println("-------------------------------")
+    for (customer in customers) {
+        println(customer)
+    }
+    println()
+
+    // fetch an individual customer by ID
+    val customer = repository.findById(1L)
+    println("Customer found with findOne(1L):")
+    println("--------------------------------")
+    println(customer)
+    println()
+
+    // fetch customers by last name
+    val bauers = repository.findByLastName("Bauer")
+    println("Customer found with findByLastName('Bauer'):")
+    println("--------------------------------------------")
+    for (bauer in bauers) {
+        println(bauer)
     }
 
-
-}
-fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args)
+    context.close()
 }
 
 
