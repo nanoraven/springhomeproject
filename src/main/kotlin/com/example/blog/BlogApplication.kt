79366a666cc868f06a7fb3a36f16dbb6be17209f
SubbotinDmitriy <jetixhunterrollx@gmail.com>
2018-03-24 13:22:18 +0700

    set

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -2,10 +2,23 @@ package com.example.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RequestMapping
 
+
+@Controller
 @SpringBootApplication
-class BlogApplication
+class BlogApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    fun home(): String {
+        return "Hello World!"
+    }
 
-fun main(args: Array<String>) {
-    runApplication<BlogApplication>(*args)
+    fun main(args: Array<String>) {
+        runApplication<BlogApplication>(*args)
+    }
 }
+
+
