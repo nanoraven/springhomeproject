4dd18fa2897382e7de73f9124e33f41f0d289335
SubbotinDmitriy <jetixhunterrollx@gmail.com>
2018-03-24 14:33:46 +0700

    set

--- src/main/kotlin/com/example/blog/hello/CustomerRepository.kt
+++ src/main/kotlin/com/example/blog/hello/CustomerRepository.kt
@@ -2,7 +2,7 @@ package com.example.blog.hello
 
 import org.springframework.data.repository.CrudRepository
 
-class CustomerRepository: CrudRepository<Customer, Long> {
+interface CustomerRepository: CrudRepository<Customer, Long> {
 
     fun findByLastName(lastName:String):List<Customer>
 }
